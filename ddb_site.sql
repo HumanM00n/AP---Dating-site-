37bd100cb922a68e11040bf2aedffea022f3aeda
Christina GAPY <114501721+christinaa0@users.noreply.github.com>
2023-07-04 15:28:20 +0200

    Update ddb_site.sql

--- ddb_site.sql
+++ ddb_site.sql
@@ -1,49 +1,37 @@
 CREATE DATABASES Adopte_GT
-CREATE TABLE Destination(
-   ID_destination VARCHAR(50),
-   Pays VARCHAR(50) NOT NULL,
-   Ville VARCHAR(50) NOT NULL,
-   Date_voyage DATE NOT NULL,
-   PRIMARY KEY(ID_destination)
-);
 
 CREATE TABLE Utilisateur(
-   ID_utilisateur VARCHAR(50),
-   Nom VARCHAR(20) NOT NULL,
-   Prenom VARCHAR(20) NOT NULL,
-   Pseudo VARCHAR(20) NOT NULL,
-   Genre VARCHAR(20) NOT NULL,
-   Email INT NOT NULL,
-   Téléphone VARCHAR(10) NOT NULL,
+   ID_utilisateur VARCHAR(200),
+   Nom VARCHAR(200) NOT NULL,
+   Prenom VARCHAR(200) NOT NULL,
+   Pseudo VARCHAR(200) NOT NULL,
+   Genre VARCHAR(200) NOT NULL,
+   Email VARCHAR(200) NOT NULL,
+   Téléphone VARCHAR(200) NOT NULL,
    Photo BLOB,
-   Centre_interet VARCHAR(100),
-   Pays_visite VARCHAR(50),
-   Pays_a_visiter VARCHAR(50) NOT NULL,
-   ID_destination VARCHAR(50) NOT NULL,
-   PRIMARY KEY(ID_utilisateur),
-   FOREIGN KEY(ID_destination) REFERENCES Destination(ID_destination)
+   Centre_interet VARCHAR(200),
+   Pays_visite VARCHAR(200),
+   Pays_a_visiter VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE Discussion(
-   ID_discussion VARCHAR(50),
-   Contenu_message VARCHAR(50),
-   ID_destination VARCHAR(50) NOT NULL,
+   ID_discussion VARCHAR(200),
+   Contenu_message VARCHAR(200),
+   ID_destination VARCHAR(200) NOT NULL,
    PRIMARY KEY(ID_discussion),
-   UNIQUE(ID_destination),
-   FOREIGN KEY(ID_destination) REFERENCES Destination(ID_destination)
-);
+  );
 
 CREATE TABLE Matchs(
-   ID_match VARCHAR(50),
-   ID_discussion VARCHAR(50) NOT NULL,
+   ID_match VARCHAR(200),
+   ID_discussion VARCHAR(200) NOT NULL,
    PRIMARY KEY(ID_match),
    UNIQUE(ID_discussion),
    FOREIGN KEY(ID_discussion) REFERENCES Discussion(ID_discussion)
 );
 
 CREATE TABLE Matcher(
-   ID_utilisateur VARCHAR(50),
-   ID_match VARCHAR(50),
+   ID_utilisateur VARCHAR(200),
+   ID_match VARCHAR(200),
    PRIMARY KEY(ID_utilisateur, ID_match),
    FOREIGN KEY(ID_utilisateur) REFERENCES Utilisateur(ID_utilisateur),
    FOREIGN KEY(ID_match) REFERENCES Matchs(ID_match)
