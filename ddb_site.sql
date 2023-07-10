dbf097a718a24283b4483522a4ba01046ff0e1c7
HumanM00n <113510790+HumanM00n@users.noreply.github.com>
2023-07-11 01:50:11 +0200

    Update ddb_site.sql

--- ddb_site.sql
+++ ddb_site.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE Adopte_GT;
 
-use adopte_gt;
+USE Adopte_GT;
 
-CREATE TABLE Utilisateur(
+CREATE TABLE Utilisateur (
    ID_utilisateur INT AUTO_INCREMENT PRIMARY KEY,
    Nom VARCHAR(200) NOT NULL,
    Prenom VARCHAR(200) NOT NULL,
@@ -17,25 +17,23 @@ CREATE TABLE Utilisateur(
    Bio VARCHAR(255)
 );
 
-CREATE TABLE Discussion(
-   ID_discussion VARCHAR(200),
+CREATE TABLE Discussion (
+   ID_discussion INT AUTO_INCREMENT PRIMARY KEY,
    Contenu_message VARCHAR(200),
-   ID_destination VARCHAR(200) NOT NULL,
-   PRIMARY KEY(ID_discussion)
-  );
+   ID_destination INT NOT NULL,
+   FOREIGN KEY(ID_destination) REFERENCES Utilisateur(ID_utilisateur)
+);
 
-CREATE TABLE Matchs(
-   ID_match VARCHAR(200),
-   ID_discussion VARCHAR(200) NOT NULL,
-   PRIMARY KEY(ID_match),
-   UNIQUE(ID_discussion),
+CREATE TABLE Matchs (
+   ID_match INT AUTO_INCREMENT PRIMARY KEY,
+   ID_discussion INT NOT NULL,
    FOREIGN KEY(ID_discussion) REFERENCES Discussion(ID_discussion)
 );
 
-CREATE TABLE Matcher(
-   ID_utilisateur VARCHAR(200),
-   ID_match VARCHAR(200),
-   PRIMARY KEY(ID_utilisateur, ID_match),
+CREATE TABLE Matcher (
+   ID_matcher INT AUTO_INCREMENT PRIMARY KEY,
+   ID_utilisateur INT,
+   ID_match INT,
    FOREIGN KEY(ID_utilisateur) REFERENCES Utilisateur(ID_utilisateur),
    FOREIGN KEY(ID_match) REFERENCES Matchs(ID_match)
 );
